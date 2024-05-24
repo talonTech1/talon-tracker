a225e63672f16f60e9fa20737eee41d8ecb9b78e
talonTech1 <smcs2026.talontech@gmail.com>
2024-05-23 22:02:51 -0400

    git commit -m rought

--- helpme.py
+++ helpme.py
@@ -1,8 +1,26 @@
 from pymongo import MongoClient
+from datetime import datetime
 cluster = MongoClient ("mongodb+srv://smcs2026talontech:lUxhcscK1PDAhJxm@talontracker.k6uzv05.mongodb.net/?retryWrites=true&w=majority&appName=TalonTracker")
 db = cluster["Tracker"]
-fav = db["Favorites"]
+locs = db["Locations"]
+def addLoc(locationN, fav = False):
+    if checkIfExisting:
+        return False
+    n = locationN.upper()
+    d = datetime.timestamp(datetime.now())
+    add = {"locN":n, "time":d, "f":fav}
+    locs.insert_one(add)
+    return True
+def getAllLocs():
+    return [i for i in list(locs.find())]
+def checkIfExisting(locationN):
+    locationN = locationN.upper()
+    check = locs.find_one({"locN":locationN})
+    return locs.find_one({"locN":locationN}) != None
 
-post = {"name:" : "bobby", "score" : 12}
-
-fav.insert_one(post)
+if input() == "a":
+    addLoc("rm23321")
+    addLoc("media center")
+print(getAllLocs())
+print(checkIfExisting("rm23321"))
+cluster.close()
