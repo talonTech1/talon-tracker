448b73952b614d1975739430ad8278d7e7421197
talonTech1 <smcs2026.talontech@gmail.com>
2024-05-23 22:26:00 -0400

    git commit -m more1

--- helpme.py
+++ helpme.py
@@ -3,12 +3,20 @@ from datetime import datetime
 cluster = MongoClient ("mongodb+srv://smcs2026talontech:lUxhcscK1PDAhJxm@talontracker.k6uzv05.mongodb.net/?retryWrites=true&w=majority&appName=TalonTracker")
 db = cluster["Tracker"]
 locs = db["Locations"]
+#db. collection. updateOne()
 def addLoc(locationN, fav = False):
-    if checkIfExisting:
-        return False
     n = locationN.upper()
+    if checkIfExisting:
+        locs.update_one({"locN":locationN}, {"$set":{"count":1}})
+        return False
     d = datetime.timestamp(datetime.now())
-    add = {"locN":n, "time":d, "f":fav}
+    c = 0
+    add = {
+        "locN":n,
+        "time":d,
+        "count":c,
+        "f":fav
+    }
     locs.insert_one(add)
     return True
 def getAllLocs():
@@ -17,10 +25,16 @@ def checkIfExisting(locationN):
     locationN = locationN.upper()
     check = locs.find_one({"locN":locationN})
     return locs.find_one({"locN":locationN}) != None
-
+def sortByRecent():
+    all = getAllLocs()
+    return sorted(all, key=lambda x: x["time"])
+def sortbyUsage():
+    all = getAllLocs()
+    return sorted(all, key=lambda x: x["usage"])
 if input() == "a":
     addLoc("rm23321")
     addLoc("media center")
 print(getAllLocs())
+print(sortByRecent())
 print(checkIfExisting("rm23321"))
 cluster.close()
