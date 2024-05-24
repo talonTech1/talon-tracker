83645c177d01be0ea36096e9d1f09257420befef
talonTech1 <smcs2026.talontech@gmail.com>
2024-05-23 22:35:28 -0400

    git commit -m working usage count

--- helpme.py
+++ helpme.py
@@ -6,24 +6,23 @@ locs = db["Locations"]
 #db. collection. updateOne()
 def addLoc(locationN, fav = False):
     n = locationN.upper()
-    if checkIfExisting:
-        locs.update_one({"locN":locationN}, {"$set":{"count":1}})
+    if checkIfExisting(locationN):
+        print("exists")
+        currentCount = locs.find_one({"locN":n})["count"]
+        locs.update_one({"locN":n}, {"$set":{"count":currentCount + 1}})
         return False
     d = datetime.timestamp(datetime.now())
-    c = 0
-    add = {
-        "locN":n,
-        "time":d,
-        "count":c,
-        "f":fav
-    }
+    add = {"locN":n,"time":d,"count":0,"f":fav}
     locs.insert_one(add)
     return True
 def getAllLocs():
     return [i for i in list(locs.find())]
+def getFavorites():
+    return [j for j in list(locs.find({"f":True}))]
 def checkIfExisting(locationN):
     locationN = locationN.upper()
     check = locs.find_one({"locN":locationN})
+    print("check",check, check == None)
     return locs.find_one({"locN":locationN}) != None
 def sortByRecent():
     all = getAllLocs()
@@ -32,6 +31,7 @@ def sortbyUsage():
     all = getAllLocs()
     return sorted(all, key=lambda x: x["usage"])
 if input() == "a":
+    print("cha cha")
     addLoc("rm23321")
     addLoc("media center")
 print(getAllLocs())
