e6857075f61b6308cf33781452aa40d00c75e898
talonTech1 <smcs2026.talontech@gmail.com>
2024-05-23 21:09:37 -0400

    git commit -m figured out databases

--- helpme.py
+++ helpme.py
@@ -1,1 +1,8 @@
-print("AYUDAME")
+from pymongo import MongoClient
+cluster = MongoClient ("mongodb+srv://smcs2026talontech:lUxhcscK1PDAhJxm@talontracker.k6uzv05.mongodb.net/?retryWrites=true&w=majority&appName=TalonTracker")
+db = cluster["Tracker"]
+fav = db["Favorites"]
+
+post = {"name:" : "bobby", "score" : 12}
+
+fav.insert_one(post)
