e02d523434e0a72fe4bf2da630a03c4988fe7aa9
talonTech1 <smcs2026.talontech@gmail.com>
2024-05-23 23:04:31 -0400

    favorites working

--- helpme.py
+++ helpme.py
@@ -1,17 +1,21 @@
 from pymongo import MongoClient
 from datetime import datetime
-cluster = MongoClient ("mongodb+srv://smcs2026talontech:lUxhcscK1PDAhJxm@talontracker.k6uzv05.mongodb.net/?retryWrites=true&w=majority&appName=TalonTracker")
+cluster = MongoClient("mongodb+srv://smcs2026talontech:lUxhcscK1PDAhJxm@talontracker.k6uzv05.mongodb.net/?retryWrites=true&w=majority&appName=TalonTracker")
 db = cluster["Tracker"]
 locs = db["Locations"]
-#db. collection. updateOne()
+
+def removeLoc(locationN):
+    locs.delete_one({"locN" : locationN.upper()})
 def addLoc(locationN, fav = False):
     n = locationN.upper()
+    d = datetime.timestamp(datetime.now())
+
     if checkIfExisting(locationN):
-        print("exists")
         currentCount = locs.find_one({"locN":n})["count"]
         locs.update_one({"locN":n}, {"$set":{"count":currentCount + 1}})
+        locs.update_one({"locN": n}, {"$set": {"time":d}})
         return False
-    d = datetime.timestamp(datetime.now())
+
     add = {"locN":n,"time":d,"count":0,"f":fav}
     locs.insert_one(add)
     return True
@@ -22,7 +26,7 @@ def getFavorites():
 def checkIfExisting(locationN):
     locationN = locationN.upper()
     check = locs.find_one({"locN":locationN})
-    print("check",check, check == None)
+    #print("check",check, check == None)
     return locs.find_one({"locN":locationN}) != None
 def sortByRecent():
     all = getAllLocs()
@@ -30,11 +34,14 @@ def sortByRecent():
 def sortbyUsage():
     all = getAllLocs()
     return sorted(all, key=lambda x: x["usage"])
+def sortAlpha():
+    all = getAllLocs()
+    sorted(all, key=lambda x: x['locN'])
 if input() == "a":
     print("cha cha")
-    addLoc("rm23321")
-    addLoc("media center")
+    addLoc(input(),True)
 print(getAllLocs())
+print(getFavorites())
 print(sortByRecent())
 print(checkIfExisting("rm23321"))
 cluster.close()
